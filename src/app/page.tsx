0bd07f5a3e8a1b4fb6e5a7bc4892358b6ad38d21
Brian <galyenb@vcu.edu>
2024-03-23 22:44:36 -0400

    added bg img and styling TODO: finish planning/pick ui library

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,24 @@
-import Image from "next/image";
+import Image from 'next/image';
 
 export default function Home() {
   return (
     <main className="relative">
-      <div className="">
-        {/* Unable to get opacity to work */}
-        <img
-          className="h-screen w-full object-cover bg-opacity-5 "
-          src="/myroad.jpeg"
-        ></img>
-      </div>
-
+      {/* do not use bg-opacity-# when styling an image that is full wid/height, just use opacity-# */}
+      <Image
+        className="h-screen w-full object-cover opacity-70 "
+        src="/myroad.jpeg"
+        width={500}
+        height={500}
+        alt="road background image"
+      />
       <section>
-        <h2>Features</h2>Lorem ipsum dolor sit amet consectetur adipisicing
-        elit. Velit odit quos fuga sint iure dolor similique nesciunt totam,
-        fugiat incidunt blanditiis, harum non maiores laudantium tenetur nulla
-        commodi quae in?
+        <h2>Features</h2>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit odit
+          quos fuga sint iure dolor similique nesciunt totam, fugiat incidunt
+          blanditiis, harum non maiores laudantium tenetur nulla commodi quae
+          in?
+        </p>
       </section>
     </main>
   );
